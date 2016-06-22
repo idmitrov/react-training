5a5ce6b6995178e06a09b6298ab5b9145506eb20
Dimitar.B.Dimitrov <dimitar.b.dimitrov@stanga.net>
2016-06-22 11:47:19 +0300

    using actions/reducers/store

--- React - Redux/src/index.js
+++ React - Redux/src/index.js
@@ -1,10 +1,15 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
+import configureStore from './store/configureStore';
 import routes from './routes';
 
+const store = configureStore();
 render(
-	<Router history={browserHistory} routes={routes} />,
+  <Provider store={store} >
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
 	document.getElementById('app')
 );
